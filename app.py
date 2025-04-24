157a5ad915d3125d8ec646595113fd3c600c11b4
AadityaPradhan-8584 <82425470+AadityaPradhan-8584@users.noreply.github.com>
2025-04-24 08:50:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
 import pickle
 import re
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # --- Load model and tokenizer ---
-model = tf.keras.models.load_model("sentiment_model.h5")
+model = load_model("sentiment_model.h5")
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
 
